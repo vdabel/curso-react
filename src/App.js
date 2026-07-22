5e3f0fbed6c5ef2729253a39be191c73bb6945a1
Abel Valdivia <abelenriquevaldivia@gmail.com>
2026-07-21 21:30:26 -0500

    Video 04

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './platzi.webp';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
 import './App.css';
+import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el curso de Intro a React.js', completed: false },
+  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'JAJAJJAJAJ', completed: false }                                           
+]                                                                                                                                                                                                                                                                                                                                                                                                                 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+      <TodoCounter completed={16} total={25} />
+      <TodoCounter completed={8} total={12} />
+      <TodoCounter completed={3} total={5} />
+      <TodoSearch />
+
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text} 
+            completed={todo.completed} 
+          />
+        ))}
+      </TodoList>
+      
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
