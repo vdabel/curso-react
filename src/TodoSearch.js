96225ef2cb94adf0267d8d4761a227529ccaf5d7
Abel Valdivia <abelenriquevaldivia@gmail.com>
2026-07-27 00:16:23 -0500

    video 06

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,17 @@ import './TodoSearch.css';
 
 function TodoSearch() {
   return (
-    <input id="search" placeholder="Cortar cebolla" className="TodoSearch" />
+    <input 
+      id="search" 
+      placeholder="Cortar cebolla" 
+      className="TodoSearch" 
+      onChange={(event) => {
+        console.log('Escribiste en el TodoSearch');
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
+    />
   );
 }
 
