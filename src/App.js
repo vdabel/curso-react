444769a6b5a87cf0d6e80a0232857ef5c0394682
Abel Valdivia <132954955+vdabel@users.noreply.github.com>
2026-08-03 16:07:33 -0500

    Video 09

--- src/App.js
+++ src/App.js
@@ -21,6 +21,12 @@ function App() {
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  const searchedTodos = todos.filter(todo => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+  });
+
   return (
     <React.Fragment>
 
@@ -31,7 +37,7 @@ function App() {
       />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
             key={todo.text} 
             text={todo.text} 
