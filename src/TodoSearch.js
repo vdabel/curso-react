387e03bb61baad5ad6cca25bb752949f6232a80f
Abel Valdivia <132954955+vdabel@users.noreply.github.com>
2026-08-03 02:17:40 -0500

    video 08

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,7 @@
-import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState('');
+function TodoSearch({ searchValue, setSearchValue }) {
 
-  console.log('Los usuarios buscan todos de ' + searchValue);
   return (
     <input 
       id="search" 
