d53a897e8d163c0687707465f2f4f7c0a018b409
Abel Valdivia <132954955+vdabel@users.noreply.github.com>
2026-08-10 20:42:35 -0500

    Video 12

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,21 +1,25 @@
+import { CompleteIcon } from './CompleteIcon';
+import { DeleteIcon } from './DeleteIcon';
 import './TodoItem.css';
 
 function TodoItem(props) {
   return (
+
+    
+
+
+
     <li className="TodoItem">
-      <span 
-        onClick={props.onComplete} 
-        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
-      >
-        V
-      </span>
+      <CompleteIcon
+        completed={props.completed}
+        onComplete={props.onComplete} 
+      />
+
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
-      <span
-        onClick={props.onDelete}
-        className={`Icon Icon-delete`}
-      >
-        X
-      </span>
+      
+      <DeleteIcon
+        onDelete={props.onDelete}
+      />
     </li>
   );
 }
