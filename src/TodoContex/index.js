958a54dd5bea8fbdaec5c10f72cc60bde0ca6a1e
Abel Valdivia <132954955+vdabel@users.noreply.github.com>
2026-09-14 01:18:59 -0500

    Video 27

--- src/TodoContex/index.js
+++ src/TodoContex/index.js
@@ -25,6 +25,15 @@ function TodoProvider({ children }) {
     return todoText.includes(searchText);
   });
 
+  const addTodo = (text) => {
+    const newTodos = [...todos];
+    newTodos.push({
+      text,
+      completed: false
+    });
+    saveTodos(newTodos);
+  }
+
   const completeTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(todo => todo.text === text);
@@ -39,6 +48,8 @@ function TodoProvider({ children }) {
     saveTodos(newTodos);
   }
 
+  
+
   return (
     <TodoContext.Provider value={{
       loading,
@@ -51,7 +62,8 @@ function TodoProvider({ children }) {
       completeTodo,
       deleteTodo,
       openModal,
-      setOpenModal
+      setOpenModal,
+      addTodo
     }}>
       {children}
     </TodoContext.Provider>
--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import './TodoForm.css';
+import { TodoContext } from '../TodoContex';
 
 
 function TodoForm() {
+  const [newTodoValue, setNewTodoValue] = React.useState('');
+
+  const { 
+    addTodo,
+    setOpenModal 
+  } = React.useContext(TodoContext);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    // Si newTodoValue existe en Todos
+    // entonces, mostrar una alerta y limpiar el textarea
+
+    
+    addTodo(newTodoValue.trim());
+    setOpenModal(false);
+  };
+
+  const onCancel = () => {
+    setOpenModal(false);
+  };
+
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
+  }
+
   return (
-    <form onSubmit={(event) => {
-      event.preventDefault(); // Evita que la pagina se recarga, es decir, evita la accion por defecto. 
-    }}>
+    <form onSubmit={onSubmit}>
       <label>Escribe tu nuevo TODO</label>
-      <textarea placeholder="Cortar cebolla para el almuerzo" />
+      <textarea 
+        placeholder="Cortar cebolla para el almuerzo"
+        required
+        value={newTodoValue}
+        onChange={onChange}
+      />
       <div className="TodoForm-buttonContainer">
-        <button type="" className="TodoForm-button TodoForm-button--cancel">Cancelar</button>
-        <button type="" className="TodoForm-button TodoForm-button--add">Añadir</button>
+        <button type="button" className="TodoForm-button TodoForm-button--cancel"
+        onClick={onCancel}>Cancelar</button>
+        <button type="submit" className="TodoForm-button TodoForm-button--add">Añadir</button>
       </div>
     </form>
   );
